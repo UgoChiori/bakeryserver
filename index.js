3903a4afd302deb6c3e088865e05c0e0209afef7
Ugo Chiori <ugochiori@gmail.com>
2023-07-26 12:12:14 +0100

    server

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(cors());
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.get("/api/maps/place", async (req, res) => {
+  const { latitude, longitude, radius, type } = req.query;
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=bakery,bakers,cakes,cafe&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something is wrong" });
+  }
+});
+
+app.get("/api/maps/directions", async (req, res) => {
+  const { origin, destination } = req.query;
+  const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something is wrong" });
+  }
+});
+
+app.get("/api/maps/place/details", async (req, res) => {
+  const { place_id } = req.query;
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something is wrong" });
+  }
+});
+
+app.get("/api/maps/place/next", async (req, res) => {
+  const { pagetoken } = req.query;
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${pagetoken}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something is wrong" });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    });
