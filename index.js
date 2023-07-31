4815a5abb931574baf89b41b5e8f43cc79ece2d1
Ugo Chiori <ugochiori@gmail.com>
2023-07-31 10:31:44 +0100

    nextpagetoken added to the list of tokens

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.get("/", (req, res) => {
 app.get("/api/maps/place", async (req, res) => {
   const { latitude, longitude, radius } = req.query;
   const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=cake,coffee,dessert,bakers,bakery&key=${process.env.GOOGLE_MAPS_API_KEY}&location=${latitude},${longitude}&radius=${radius}`;
+
+if(nextPageToken){
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${nextPageToken}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+}
+
     try {
     const response = await fetch(url);
     const data = await response.json();
